ad07c41fc0b5dd802bac8f98d405b1579fe022eb
Daniel Noworyta <daniel.noworyta@gmail.com>
2023-10-29 11:55:22 +0100

    Fix links types.

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,10 +1,8 @@
-import { type UrlObject } from "url";
-
 import * as NextLink from "next/link";
 
-import { type Link } from "./header";
+import { type ILink } from "./header";
 
-export const Navbar = ({ links }: { links: Link[] }) => {
+export const Navbar = ({ links }: { links: ILink[] }) => {
 	return (
 		<nav className="relative hidden gap-4 text-xl font-normal lg:block">
 			<div className="flex gap-2">
